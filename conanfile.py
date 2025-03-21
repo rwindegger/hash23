154f4405e7d0ea2355ccee625b1121dec9e43d1c
Rene Windegger <rene@windegger.wtf>
2025-03-21 19:16:50 +0100

    Set version to 1.0

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 
 class hash23(ConanFile):
     name = "hash23"
-    version = "2.0"
+    version = "1.0"
 
     license = "MIT"
     author = "Rene Windegger <rene@windegger.wtf>"
