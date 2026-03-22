677b929eb1ca2c8ae536dc6f91f152f22ff96d6f
Rene Windegger <rene@windegger.wtf>
2026-03-22 12:24:52 +0100

    Add string and empty input tests for FNV-1 and FNV-1a hash implementations

--- tests/fnv_1_tests.cpp
+++ tests/fnv_1_tests.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <hash23/hash23.h>
+#include <string>
 
 namespace {
     TEST(fnv_1, single_block_test) {
@@ -13,6 +14,28 @@ namespace {
             EXPECT_EQ(expected, actual);
         } else {
             constexpr std::size_t expected = 0x7b5ea4c513c14886uz;
+            EXPECT_EQ(expected, actual);
+        }
+    }
+
+    TEST(fnv_1, string_test) {
+        constexpr auto actual = hash23::fnv_1::calculate(std::string{"Hello, World!"});
+        if constexpr (sizeof(std::size_t) == 4) {
+            constexpr std::size_t expected = 0x4291a886uz;
+            EXPECT_EQ(expected, actual);
+        } else {
+            constexpr std::size_t expected = 0x7b5ea4c513c14886uz;
+            EXPECT_EQ(expected, actual);
+        }
+    }
+
+    TEST(fnv_1, empty_input_test) {
+        constexpr auto actual = hash23::fnv_1::calculate("");
+        if constexpr (sizeof(std::size_t) == 4) {
+            constexpr std::size_t expected = 0x811c9dc5uz;
+            EXPECT_EQ(expected, actual);
+        } else {
+            constexpr std::size_t expected = 0xcbf29ce484222325uz;
             EXPECT_EQ(expected, actual);
         }
     }
--- tests/fnv_1a_tests.cpp
+++ tests/fnv_1a_tests.cpp
@@ -16,4 +16,26 @@ namespace {
             EXPECT_EQ(expected, actual);
         }
     }
+
+    TEST(fnv_1a, string_test) {
+        constexpr auto actual = hash23::fnv_1a::calculate(std::string{"Hello, World!"});
+        if constexpr (sizeof(std::size_t) == 4) {
+            constexpr std::size_t expected = 0x5aecf734uz;
+            EXPECT_EQ(expected, actual);
+        } else {
+            constexpr std::size_t expected = 0x6ef05bd7cc857c54uz;
+            EXPECT_EQ(expected, actual);
+        }
+    }
+
+    TEST(fnv_1a, empty_input_test) {
+        constexpr auto actual = hash23::fnv_1a::calculate("");
+        if constexpr (sizeof(std::size_t) == 4) {
+            constexpr std::size_t expected = 0x811c9dc5uz;
+            EXPECT_EQ(expected, actual);
+        } else {
+            constexpr std::size_t expected = 0xcbf29ce484222325uz;
+            EXPECT_EQ(expected, actual);
+        }
+    }
 }
